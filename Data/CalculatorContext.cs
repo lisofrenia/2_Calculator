b0d3bd6ff7509aacd221e315aa57e4f21e2908d9
lisofrenia <chesharisa666@gmail.com>
2025-12-18 13:15:32 +0300

    fix

---
+++ Data/CalculatorContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Data
+{
+    public class CalculatorContext : DbContext
+    {
+        public DbSet<DataInputVarian> DataInputVarians { get; set; }
+
+        public CalculatorContext(DbContextOptions<CalculatorContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Data/DataInputVarian.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication2.Data
+{
+    public class DataInputVarian
+    {
+        [Key]
+        public int ID_DataInputVarian { get; set; }
+
+        [Column(TypeName = "varchar(128)")]
+        public string? Operand_1 { get; set; }
+
+        [Column(TypeName = "varchar(128)")]
+        public string? Operand_2 { get; set; }
+
+        [Column(TypeName = "varchar(128)")]
+        public string? Type_operation { get; set; }
+
+        [Column(TypeName = "varchar(128)")]
+        public string? Result { get; set; }
+    }
+}
+   
