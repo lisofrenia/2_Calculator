5c7e4b5c55733ac596a1882b82ed4026d3ba5ce3
lisofrenia <chesharisa666@gmail.com>
2025-12-22 15:13:14 +0300

    fix

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<CalculatorContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
 
